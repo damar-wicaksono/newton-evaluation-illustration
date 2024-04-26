7c60e63ea18d0565f3826fbad1e76a737bb8050e
Damar Wicaksono <damar.wicaksono@outlook.com>
2024-04-26 09:01:24 +0200

    Add the revised version of the splitting.
    - Move the explicit data copy to the device
      out of the loop.

---
+++ src/newton_eval/numba_gpu_splits_rev.py
@@ -0,0 +1,149 @@
+import numba
+import numpy as np
+
+from math import ceil
+from numba import cuda
+
+INT_DTYPE = np.int_
+FLOAT_DTYPE = np.float_
+
+FLOAT = numba.from_dtype(FLOAT_DTYPE)
+F_1D = FLOAT[:]
+F_2D = FLOAT[:, :]
+
+INT = numba.from_dtype(INT_DTYPE)
+I_1D = INT[:]
+I_2D = INT[:, :]
+
+__all__ = ["eval_driver_numba_gpu_splits_rev"]
+
+
+@cuda.jit(device=True, inline=True, lineinfo=True)
+def compute_newton_monomial(
+    x_single: np.ndarray,
+    exponent_single: np.ndarray,
+    generating_points: np.ndarray,
+):
+    """Precompute the value of a Newton monomial at a single point.
+
+    Notations used below:
+
+    - ``m``: spatial dimension
+    - ``n``: the polynomial degree
+    - ``N``: the number of monomials (the cardinality of the multi-index set)
+
+    Parameters
+    ----------
+    x_single : np.ndarray
+        A single point, an array of length ``m``.
+    exponent_single : np.ndarray
+        Array single multidimensional exponent of length ``m``.
+    generating_points : np.ndarray
+        Array of points to generate the grid;
+        the array is of shape ``(n-by-m)``.
+    """
+    m = len(exponent_single)
+    tmp = 1.0
+    for i in range(m):
+        for j in range(exponent_single[i]):
+            p_ij = generating_points[j, i]
+            tmp *= x_single[i] - p_ij
+
+    return tmp
+
+
+@cuda.jit(inline=True, lineinfo=True)
+def eval_newton_polynomial(
+    xx: np.ndarray,
+    exponents: np.ndarray,
+    generating_points: np.ndarray,
+    newton_coefficients: np.ndarray,
+    output_placeholder: np.ndarray,
+    starts_ends: np.ndarray,
+    idx: int,
+) -> None:
+    """Evaluate the Newton polynomial multiple query points (CUDA kernel)."""
+    row = cuda.grid(1)
+    N = exponents.shape[0]
+    start_0 = starts_ends[idx][0]
+    end_0 = starts_ends[idx][1]
+    if row < len(output_placeholder):
+        tmp = 0.0
+        x_single = xx[row]
+
+        upper_bound = end_0 - start_0
+
+        for i in range(upper_bound):
+            i = np.int32(i)
+            idx = start_0 + i
+            newton_monomials = compute_newton_monomial(x_single, exponents[idx], generating_points)
+            tmp += newton_monomials * newton_coefficients[idx]
+
+        output_placeholder[row] += tmp
+
+
+def eval_driver_numba_gpu_splits_rev(
+    xx: np.ndarray,
+    coefficients: np.ndarray,
+    exponents: np.ndarray,
+    generating_points: np.ndarray,
+    threads_per_block: int = 256,
+    splits: int = 5,
+):
+    """Numba CUDA driver function to evaluate a polynomial in Newton form."""
+    # Common data
+    nr_points = xx.shape[0]
+
+    # Copy arrays to the device
+    xx_global_mem = cuda.to_device(xx)
+    exponents_global_mem = cuda.to_device(exponents)
+    generating_points_global_mem = cuda.to_device(generating_points)
+    coefficients_global_mem = cuda.to_device(coefficients)
+
+    # Allocate memory on the device for results
+    output_global_mem = cuda.device_array((nr_points))
+
+    # Configure the blocks
+    blocks_per_grid = int(ceil(xx.shape[0] / threads_per_block))
+
+    N = len(exponents)
+    chunk = int(N / splits)
+
+    # Start ends array
+    starts_ends = np.empty((splits, 2), dtype=np.int32)
+    for i in range(splits):
+        start = np.array([i*chunk], dtype=np.int32)
+        end = np.array([(i + 1)*chunk], dtype=np.int32)
+
+        if end > N:
+            end = N
+
+        starts_ends[i][0] = start
+        starts_ends[i][1] = end
+    starts_ends_global_mem = cuda.to_device(starts_ends)
+
+    # Start the kernel
+    for i in range(splits):
+        # start = np.array([i*chunk], dtype=np.int32)
+        # end = np.array([(i + 1)*chunk], dtype=np.int32)
+
+        # if end > N:
+        #     end = N
+
+        # start_global_mem = cuda.to_device(start)
+        # end_global_mem = cuda.to_device(end)
+
+        eval_newton_polynomial[blocks_per_grid, threads_per_block](
+            xx_global_mem,
+            exponents_global_mem,
+            generating_points_global_mem,
+            coefficients_global_mem,
+            output_global_mem,
+            starts_ends_global_mem,
+            i,
+        )
+
+    # Copy the result back to the host
+    output_placeholder = output_global_mem.copy_to_host()
+
+    return output_placeholder
---
+++ timeit-scripts/timeit_gpu_splits_rev.py
@@ -0,0 +1,38 @@
+import numpy as np
+import minterpy as mp
+import timeit
+import datetime
+import sys
+
+if __name__ == "__main__":
+
+    poly_degree = int(sys.argv[-2])
+    splits = int(sys.argv[-1])
+
+    SETUP_CODE = \
+        f"import minterpy as mp\n" \
+        f"import numpy as np\n" \
+        f"from newton_eval import eval_driver_numba_gpu_splits_rev\n" \
+        f"spatial_dimension = 4\n" \
+        f"poly_degree = {poly_degree}\n"\
+        f"mi = mp.MultiIndexSet.from_degree(spatial_dimension, poly_degree, 2.0)\n" \
+        f"xx_test = -1 + 2 * np.random.rand(1000000, spatial_dimension)\n" \
+        f"nwt_coeffs = np.random.rand(len(mi))\n" \
+        f"exponents = mi.exponents\n" \
+        f"grd = mp.Grid(mi)\n" \
+        f"gen_points = grd.generating_points\n" \
+        f"yy_numba_gpu = eval_driver_numba_gpu_splits_rev(xx_test[:5], nwt_coeffs, exponents, gen_points, splits={splits})\n" \
+
+    n_reps = 5
+    n_loops = 2
+    t = timeit.Timer(f"eval_driver_numba_gpu_splits_rev(xx_test, nwt_coeffs, exponents, gen_points, splits={splits})", setup=SETUP_CODE).repeat(n_reps, n_loops)
+
+    t_best = np.min(t) / n_loops
+    t_worst = np.max(t) / n_loops
+    t_avg = np.mean(t) / n_loops
+    t_summary = np.array([t_worst, t_avg, t_best])
+
+    current_datetime = datetime.datetime.now()
+    timestamp = current_datetime.timestamp()
+
+    np.savetxt(f"/p/home/jusers/wicaksono1/jureca/results/gpu-timing-degree-{poly_degree}-splits-rev-{splits}-{timestamp}.txt", t_summary)
